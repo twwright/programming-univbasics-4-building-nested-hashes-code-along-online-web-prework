d5f30c35d7362a63ba4808d4794453b9601bb539
Thomas Wright <twwright@gmail.com>
2019-09-30 04:56:13 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,10 +4,13 @@ def second_challenge
       :patriach => {},
       :matriach => {},
       :hero => {},
-      :hero_friends =>
+      :hero_friends => []
       },
     :capulet => {
-      #your key/value pairs here!
+      :patriach => {},
+      :matriach => {},
+      :hero => {},
+      :hero_friends => []
       }}
 
 
