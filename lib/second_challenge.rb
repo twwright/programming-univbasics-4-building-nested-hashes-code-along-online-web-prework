e7e950ee2a19c70b11fecb93ef953af38fe3484a
Thomas Wright <twwright@gmail.com>
2019-09-30 04:55:29 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,14 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
-      }, 
+      :patriach => {},
+      :matriach => {},
+      :hero => {},
+      :hero_friends =>
+      },
     :capulet => {
       #your key/value pairs here!
       }}
 
-  
+
 end
